cc62a1f0219cb6be9c353a58a7efa2f133aa87f1
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-02-21 08:48:25 +0100

    Added import.sql to test class

--- src/test/java/org/example/kata/confectionery/service/CakeServiceTest.java
+++ src/test/java/org/example/kata/confectionery/service/CakeServiceTest.java
@@ -2,8 +2,10 @@ package org.example.kata.confectionery.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
+@Sql({"/import.sql"})
 class CakeServiceTest {
 
     @Autowired
