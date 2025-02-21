bb0a53213105a1402949d57cd14bf248eaef9be9
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-02-21 14:10:54 +0100

    Kata getCake 1, RED. Code does not even compile!

--- src/test/java/org/example/kata/confectionery/service/CakeServiceTest.java
+++ src/test/java/org/example/kata/confectionery/service/CakeServiceTest.java
@@ -1,12 +1,25 @@
 package org.example.kata.confectionery.service;
 
+import org.example.kata.confectionery.persistence.model.Cake;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CakeServiceTest {
 
     @Autowired
     private ICakeService service;
+
+    @Test
+    void when_get_cake_with_existing_id_then_return_cake() {
+        Cake cake = this.service.getCake(1L);
+
+        assertNotNull(cake);
+        assertEquals(1L, cake.getId());
+    }
 
 }
