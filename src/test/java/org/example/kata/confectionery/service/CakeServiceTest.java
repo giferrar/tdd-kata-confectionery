27ce27af7a3fac46a8628f79ef267c2d5de650b5
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-02-21 09:58:17 +0100

    Renamed import.sql to data.sql and deferred datasource initialization

--- src/test/java/org/example/kata/confectionery/service/CakeServiceTest.java
+++ src/test/java/org/example/kata/confectionery/service/CakeServiceTest.java
@@ -2,10 +2,8 @@ package org.example.kata.confectionery.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
-@Sql({"/import.sql"})
 class CakeServiceTest {
 
     @Autowired
