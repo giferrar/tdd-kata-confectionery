7e87a55259b08a22a0c644774c8ee786884c5e13
Ferrario.Gianluca <Ferrario.Gianluca@Bank-Verlag.de>
2025-02-24 13:20:55 +0100

    Kata eatCakeIfHealthy 3. GREEN. CakeNotBakedException is thrown

--- src/main/java/org/example/kata/confectionery/service/CakeService.java
+++ src/main/java/org/example/kata/confectionery/service/CakeService.java
@@ -46,7 +46,7 @@ public class CakeService implements ICakeService {
 
     @Override
     public Integer eatCakeIfHealthy(long id) {
-        Cake cake = this.cakeRepository.findById(id).get();
+        Cake cake = this.cakeRepository.findById(id).orElseThrow(() -> new CakeNotBakedException("Cake with ID %s not baked".formatted(id)));
         return cake.isVegan() ? cake.getCalories() : 0;
     }
 }
